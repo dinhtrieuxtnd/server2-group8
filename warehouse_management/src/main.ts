b164378643de06578a3b6326ae9b9ad819f31442
Dinh Cong Trieu <dinhtrieuxtnd@gmail.com>
2025-08-25 16:49:11 +0700

    set up folder structure, add middleware, add decorator @Match

--- warehouse_management/src/main.ts
+++ warehouse_management/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Tự động loại bỏ các field không được khai báo decorator trong DTO
+      forbidNonWhitelisted: true, // Nếu có field không được khai báo decorator trong DTO mà client truyền lên thì sẽ báo lỗi
+      transform: true, // Tự động chuyển đổi dữ liệu sang kiểu được khai báo trong DTO
+      transformOptions: {
+        enableImplicitConversion: true, // Cho phép chuyển đổi kiểu dữ liệu một cách ngầm định
+      },
+    })
+  )
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
